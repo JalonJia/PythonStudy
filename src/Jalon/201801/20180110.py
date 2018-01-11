fb605fc3bbb840644471fe8e662d42ecede4ce9a
JALON <JALON@JALONWin10>
2018-01-11 23:26:49 +0800

    增加了文件夹遍历

--- src/Jalon/201801/20180110.py
+++ src/Jalon/201801/20180110.py
@@ -4,6 +4,7 @@
 
 import os
 import os.path
+from os.path import join #这样引入之后，join就可以直接使用了
 
 #列出目录的文件
 sPath = "D:\ACCPAC"
@@ -50,6 +51,17 @@ sPath2 = 'D:\Test1\T2'
 print(__name__)
 
 #os.path.('D:\')
+
+print(os.walk('D:\Test'))
+#os.walk('D:\\', Null, Null, Null)
+for root, dirs, files in os.walk('D:\Test'): #files会得到目录下的文件（不包括文件夹）；dirs会获取到每个文件夹下面的子目录；root会遍历每个子文件夹
+    print(root, "consumes", end="")
+    print(files)
+    print(dirs)
+    #print(sum([os.path.getsize(join(root, name)) for name in files]), end="")
+    #print("bytes in", len(files), "non-directory files")
+    #if 'CVS' in dirs:
+    #    dirs.remove('CVS')  # don't visit CVS directories
 
 
 
