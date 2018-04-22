45a28cf69282bfb96b865f6460b14e4bc1773c94
jalon.jia <jalon.jia@hotmail.com>
2018-04-22 20:26:31 +0800

    提交Kevin的代码20180422

--- src/Kevin/201804/20180420.py
+++ src/Kevin/201804/20180420.py
@@ -1,16 +1,36 @@
 import turtle
-pen = turtle.Turtle()
 
+
+def draw_square(p, f, j):
+    if j > f:
+        p.lt(90)
+    p.fd(f)
+    p.lt(90)
+    p.fd(j)
+    p.lt(90)    
+    p.fd(f)
+    p.lt(90)
+    p.fd(j)
+
+
+def square(pen, r, t, u, j):
+    if ((r == t) and (u == j)):
+        print('能组成长方形')
+        draw_square(p = pen, f = list_input[1], j = list_input[3])        
+    elif ((r == j) and (u == t)):     
+        print('能组成长方形')
+        draw_square(p = pen, f = list_input[1], j = list_input[3])        
+    elif ((r == u) and (j == t)):         
+        print('能组成长方形')
+        draw_square(p = pen, f = r, j = j)
+    else:
+        print('对不起，这四个长度不能组成长方形')
+        
+
+    
 list_input = []
 s_input = ''
 j = 0
-
-def draw_square(p, ):
-
-
-
-
-
 
 while j < 4:
     s_input = input("请输入一个数字:")
@@ -22,26 +42,7 @@ while j < 4:
 
 print(list_input)
 
-def square(r, t, u, j):
-    if ((r == t) and (u == j)) or ((r == j) and (u == t)) or ((r == u) and (t == j)):
-        print('能组成正方形')
-    else:
-        print('对不起，这四个长度不能组成长方形')
-        
-'''   
-    if (r == j) and (u == t):
-        print('能组成正方形')
-    else:
-    
-    if (r == u) and (t == j):
-        print('能组成正方形')
-    else:
+pen = turtle.Turtle()
+square(pen = pen, r = list_input[0], t = list_input[1], u = list_input[2], j = list_input[3])
 
-    
-    if t == u:
-        print('能组成正方形')
-'''
-
-square(r = list_input[0], t = list_input[1], u = list_input[2], j = list_input[3])
-
-turtle.mainoop()
+turtle.mainloop()
---
+++ src/Kevin/201804/20180422.py
@@ -0,0 +1,27 @@
+score = 100 
+high = 180
+
+appleprice = 3.5
+weight = 7.5
+money = appleprice * weight
+
+money = money - 10
+
+high = input('请输入你的身高 ：')
+
+print('hello world') 
+
+age = 18
+print(age)
+
+name = '东哥'
+print(name)
+
+name = input('请输入名字：')
+
+print('性名：%s'%name)
+
+h = input('请输入你的年龄： ')
+h = int(h)
+if h > 18:
+    print('你可以上网吧')
