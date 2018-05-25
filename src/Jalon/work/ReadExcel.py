1e60acb421c01dbe6d3c0732d2f639a5fd7fc294
Administrator <Administrator@JALONVM7>
2018-05-25 09:15:41 +0800

    提交代码：可以从Excel生成资源了

--- src/Jalon/work/ReadExcel.py
+++ src/Jalon/work/ReadExcel.py
@@ -64,6 +64,52 @@ class ReadExcel(object):
                         print(sheet.row(row)[col].value.encode('utf-8'))
 
 
+    def read_excel_create_resource(self, to_file_name):
+        #文件位置
+        ExcelFile = xlrd.open_workbook(self.file_name)
+        
+        #获取目标EXCEL文件sheet名
+        #print('Sheets:', ExcelFile.sheet_names())
+        
+        file = open(to_file_name, 'w', encoding = 'UTF-8')
+             
+        #------------------------------------
+        #若有多个sheet，则需要指定读取目标sheet例如读取sheet2
+        #sheet2_name=ExcelFile.sheet_names()[1]
+        #------------------------------------
+        #获取sheet内容【1.根据sheet索引2.根据sheet名称】
+        #sheet=ExcelFile.sheet_by_index(1)
+        for sheet_name in ExcelFile.sheet_names():
+            sheet = ExcelFile.sheet_by_name(sheet_name)        
+            #打印sheet的名称，行数，列数
+            #print('Sheet Name:%s, Rows: %d, Columns: %d' % (sheet.name, sheet.nrows, sheet.ncols))
+            
+            if (sheet.name != 'New Tables') and (sheet.name != 'Changed Tables') :
+                continue
+            
+            table_name = ''
+             
+            #print Each Cell
+            for row in range(sheet.nrows):
+                if sheet.row_values(row)[0][:2] == 'EN' :
+                    table_name = sheet.row_values(row)[0]
+                    res_str = 'IDS_%s_VIEW_NAME       ,        "%s"\nIDS_%s_VIEW_NOUN       ,        "%s"\n' % (table_name, sheet.row_values(row)[1], table_name, sheet.row_values(row)[1])
+                    print(res_str)
+                    file.write(res_str)
+                    #print(table_name)
+                    continue
+                
+                if sheet.row_values(row)[1] != '' :
+                    #print(sheet.row_values(row)[1])
+                    #print(sheet.row_values(row)[5])
+                    res_str = 'IDS_%s_%s_FLD        ,        "%s"\n' % (table_name, sheet.row_values(row)[1], sheet.row_values(row)[5])
+                    print(res_str)
+                    file.write(res_str)
+                    
+                
+                        
+        file.close()               
+
 #def __main__():
 #    pass
 
@@ -104,6 +150,8 @@ def read_excel():
 #Testing
 if (__name__ == '__main__') :
     #print(xlrd.XL_CELL_NUMBER)
-    x = ReadExcel(r'D:\Documents\RM64ADocs\PU3\EN64APU3_TablesChange.xlsx')
-    x.read_excel()
+    x = ReadExcel(r'D:\Documents\RM65ADocs\PU0\Designment\EN65A_TablesChange.xlsx')
+    #x.read_excel()
+    x.read_excel_create_resource(r'D:\Documents\RM65ADocs\PU0\Designment\temp.txt')
+    
     
