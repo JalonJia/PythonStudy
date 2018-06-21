171ce85c55683f80e3bd25e8a0cf20fee5d9df4b
jalon <jalon@JALONJIA>
2018-06-21 19:48:52 +0800

    提交OCR测试程序

--- src/Jalon/201806/TestOCR.py
+++ src/Jalon/201806/TestOCR.py
@@ -8,6 +8,6 @@ import pytesseract
 from PIL import Image
 
 # open image
-image = Image.open('c:\\1.png')
-code = tesseract.image_to_string(image, lang='chi_sim')
+image = Image.open('c:\\x.jpg')
+code = pytesseract.image_to_string(image, lang='chi_sim')
 print(code)
