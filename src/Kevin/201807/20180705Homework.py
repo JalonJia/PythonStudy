8f66f85124d5e69ee1a58d68ea8d171c6781be7d
jalon <jalon@192.168.41.1>
2018-07-05 19:23:07 +0800

    留作业

---
+++ src/Kevin/201807/20180705Homework.py
@@ -0,0 +1,15 @@
+'''
+作业: 关于列表的一些计算
+1. 写一个函数calc_list_maxvalue(x), 传入一个全是数字的list x，函数用来计算这个list中最大的数字，打印结果并返回
+2. 写一个函数calc_list_minvalue(x), 传入一个全是数字的list x，函数用来计算这个list中最小的数字，打印结果并返回
+3. 写一个函数calc_list_summary(x), 传入一个全是数字的list x，函数用来计算这个list中所有数字的和，打印结果并返回
+4. 写一个函数calc_list_average(x), 传入一个全是数字的list x，函数用来计算这个list中所有数字的平均值，打印结果并返回
+5. 写一个函数calc_list_hasvalue(x, element), 传入一个全是数字的list x，以及另外一个数字element，
+        函数用来判断element在list x中出现的次数，打印结果并返回
+6. 写一个函数add_value_to_list(x, element), 传入一个全是数字的list x，以及另外一个数字element，
+        如果element在list x中不存在，则把它加进去，否则不添加, 打印整个列表的元素并返回
+7. 写一个函数remove_duplicate_from_list(x), 传入一个全是数字的list x，
+        用来删除列表中重复的元素，删除之后列表中所有元素都不重复, 打印删除前以及删除后的列表并返回
+        例如列表中以前有[1,2,3,1,2,3]6个元素，调用函数之后，列表中应该只剩下[1,2,3]三个元素        
+
+'''
