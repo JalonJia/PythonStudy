ace00e2992bc53b03d9fdfe9447d3b2790ca7359
jalon <jalon@JALONBJ>
2018-07-17 22:23:41 +0800

    留作业

---
+++ src/Kevin/201807/20180717Homework.py
@@ -0,0 +1,15 @@
+'''
+Created on 2018年7月17日
+作业: 关于字符串的一些计算
+1. 写一个函数find(str, substr), 传入一个string str以及一另一个字符串substr，函数用来判断字符串str中是否包含字符串substr，
+        如果包含，打印结果并返回substr在字符串str中的下标；如果不包含，返回-1
+2. 写一个函数count(str, substr), 传入一个string str以及一另一个字符串substr，函数用来计算并返回字符串str中包含字符串substr的次数，
+       
+3. 写一个函数replace(str, substr, substr2), 传入三个字符串，函数将第一个字符串str中的子字符串substr都替换成substr2, 并返回替换后的字符串.
+ 例如：调用s = replace('jiajikai jiamiaomiao jiayanlong', 'jia', 'j_')之后，s的值是'j_jikai j_miaomiao j_yanlong'
+4. 写一个函数get_chars(str), 传入一个字符串str, 返回字符串中包含的所有不重复的字符列表。
+例如：调用 x = get_chars('jiajikai')之后，x是这样的列表：['j','i','a','k'](包含了字符串的所有字母，并且不重复）。提示：调用上次写的函数。
+
+
+'''
+
